a736199f55766b85e29dee8ddc6a3a85d688cc92
xpidaGH <tuffuornana@gmail.com>
2023-02-27 02:58:37 +0000

    27/02/2023 Syncfusion Community Linsence

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
 import { registerLicense } from "@syncfusion/ej2-base";
+
 // Registering Syncfusion license key
 registerLicense(
   "ORg4AjUWIQA/Gnt2VVhkQlFadVdJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkdhW39acnBVQWNVWUU="
